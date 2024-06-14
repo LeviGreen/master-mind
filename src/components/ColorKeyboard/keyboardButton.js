d49a6ac20bac3776c64b8867a8eaa4c0fd000253
LeviGreen <lgreen.design@comcast.net>
2024-06-14 13:31:07 -0400

    add css to buttons to make them a bit more user friendly

--- src/components/ColorKeyboard/keyboardButton.js
+++ src/components/ColorKeyboard/keyboardButton.js
@@ -1,17 +1,17 @@
-import "./index.css";
 import React from "react";
+import "./index.css";
 
 function KeyboardButton({ onClick, color, text, disabled }) {
-    return (
-        <button
-            className={"keyboard-button"}
-            onClick={onClick}
-            style={{ backgroundColor: color }}
-            disabled={disabled}
-        >
-            {text}
-        </button>
-    );
+  return (
+    <button
+      className={"keyboard-button"}
+      onClick={onClick}
+      style={{ backgroundColor: color }}
+      disabled={disabled}
+    >
+      {text}
+    </button>
+  );
 }
 
 export default KeyboardButton;
