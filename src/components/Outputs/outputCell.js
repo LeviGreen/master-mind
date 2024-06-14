9fcb352911572901421c257d3778c67b076fb3bd
LeviGreen <lgreen.design@comcast.net>
2024-06-14 11:47:43 -0400

    only show OutputRow after the user guesses for that round

--- src/components/Outputs/outputCell.js
+++ src/components/Outputs/outputCell.js
@@ -1,13 +1,20 @@
 import "./index.css";
-import { BLACK, GRAY, KEY_COLORS, NULL } from "../../constants";
+import { GRAY, NULL } from "../../constants";
 
 function OutputRow({ clue }) {
+  let rowColor = clue.length === 4 && clue.every((value) => value === GRAY) ? NULL : GRAY;
   return (
-    <div className="output-row">
-      <div className="output-cell" style={{ backgroundColor: clue[0] || GRAY }}/>
-      <div className="output-cell" style={{ backgroundColor: clue[1] || GRAY }}/>
-      <div className="output-cell" style={{ backgroundColor: clue[2] || GRAY }}/>
-      <div className="output-cell" style={{ backgroundColor: clue[3] || GRAY }}/>
+    <div className="output-row" style={{ backgroundColor: rowColor }}>
+      {
+        rowColor === GRAY ? <>
+          <div className="output-cell" style={{ backgroundColor: clue[0] || GRAY }}/>
+          <div className="output-cell" style={{ backgroundColor: clue[1] || GRAY }}/>
+          <div className="output-cell" style={{ backgroundColor: clue[2] || GRAY }}/>
+          <div className="output-cell" style={{ backgroundColor: clue[3] || GRAY }}/>
+        </>
+        : null
+      }
+
     </div>
   );
 }
